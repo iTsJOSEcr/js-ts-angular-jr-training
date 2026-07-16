93c421e0be81c55c7bf16ccb4de33371faddd9d2
Jose Daniel <josedanielhernandezu2@gmail.com>
2026-07-16 12:41:22 -0600

    feat: implementar recorrido y búsqueda básica en arrays

---
+++ week-01/day-03/03-recorrer-arrays.js
@@ -0,0 +1,59 @@
+const productos = [
+    {
+        id: 1,
+        nombre: "Mouse",
+        precio: 15000,
+        stock: 10
+    },
+    {
+        id: 2,
+        nombre: "Teclado",
+        precio: 30000,
+        stock: 5
+    },
+    {
+        id: 3,
+        nombre: "Monitor",
+        precio: 120000,
+        stock: 2
+    }
+];
+
+
+
+function mostrarProductos(productos) {
+    for (let i = 0; i < productos.length; i++) {
+        console.log("producto", productos[i].nombre, 
+            "precio", productos[i].precio,
+             "stock", productos[i].stock
+            
+        )  
+    }
+}
+
+//mostrarProductos(productos);
+
+
+function mostrarPrecios(productos){
+    for (let i = 0; i < productos.length; i++){
+        console.log("precio", productos[i].precio);
+    }
+}
+
+
+//mostrarPrecios(productos);
+
+
+function buscarProducto(productos, nombreBuscado) {
+    for (let i = 0; i < productos.length; i++) {
+        if (productos[i].nombre === nombreBuscado) {
+            return productos[i];
+        }
+    }
+    return "Producto no encontrado";
+
+}
+
+const resultado = buscarProducto(productos, "Laptop");
+
+console.log(resultado);
