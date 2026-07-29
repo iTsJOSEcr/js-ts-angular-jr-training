aa15933a8c517fbfc84b730d1c2624ef4fdaf738
Jose Daniel <josedanielhernandezu2@gmail.com>
2026-07-29 14:53:27 -0600

    Semana 3 Día 1 - Tipos primitivos y arrays en TypeScript

---
+++ week-03/day-01/01-primitives.ts
@@ -0,0 +1,16 @@
+let nombre: string = "jose";
+let edad: number = 22;
+let activo: boolean = true;
+let altura: number = 1.70;
+let estudiante: boolean = true;
+let universidad = "Fidelitas";
+
+
+console.log(nombre)
+console.log(edad)
+console.log(activo)
+console.log(altura)
+console.log(estudiante)
+console.log(universidad)
+
+
---
+++ week-03/day-01/02-arrays.ts
@@ -0,0 +1,29 @@
+const numeros: number[] = [1,2,3,4,5];
+numeros.push(6);
+
+const frutas: string[] = ["Manzana", "Banano", "Uva"];
+frutas.push("Piña");
+
+
+const datos: (string | number | boolean)[] = [
+    "José",
+    22,
+    "Costa Rica",
+    2026
+];
+datos.push(true);
+datos.push("José");
+datos.push(22);
+
+datos.push("Ingeniería")
+
+datos.push(97);
+
+
+
+
+console.log(datos);
+
+console.log(frutas);
+
+console.log(numeros);
