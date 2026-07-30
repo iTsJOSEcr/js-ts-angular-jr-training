a642a5b38d269eea4e640b5af652d2468785a9c1
Jose Daniel <josedanielhernandezu2@gmail.com>
2026-07-29 19:03:45 -0600

    Aprender arrays, objetos y type en TypeScript

--- week-03/day-01/02-arrays.ts
+++ week-03/day-01/02-arrays.ts
@@ -15,15 +15,118 @@ datos.push(true);
 datos.push("José");
 datos.push(22);
 
-datos.push("Ingeniería")
+//datos.push("Ingeniería")
 
-datos.push(97);
-
-
+//datos.push(97);
 
 
-console.log(datos);
 
-console.log(frutas);
 
-console.log(numeros);
+//console.log(datos);
+
+//console.log(frutas);
+
+//console.log(numeros);
+
+
+
+
+
+
+const persona: {
+    nombre: string;
+    edad: number;
+    activo: boolean;
+} = {
+    nombre: "José",
+    edad: 22,
+    activo: true
+};
+
+//console.log(persona);
+
+
+
+
+
+const carro: {
+    marca: string;
+    modelo: string;
+    año: number;
+} = {
+    marca: "Toyota",
+    modelo: "Corolla",
+    año: 2022
+};
+
+carro.marca = "Honda";
+carro.año = 2025;
+
+//console.log(carro);
+
+
+
+
+const empleado: {
+    nombre: string;
+    edad: number;
+    activo: boolean;
+} = {
+    nombre: "Jose",
+    edad: 22,
+    activo: true
+};
+
+console.log(empleado)
+
+
+const empleado2: {
+    nombre: string;
+    edad: number;
+    activo: boolean;
+} = {
+    nombre: "Maria",
+    edad: 30,
+    activo: false
+};
+
+//console.log(empleado2)
+
+
+type Empleado = {
+    nombre: string;
+    edad: number;
+    activo: boolean;
+};
+
+const empleado3: Empleado = {
+    nombre: "José",
+    edad: 22,
+    activo: true
+};
+
+const empleado4: Empleado = {
+    nombre: "María",
+    edad: 30,
+    activo: false
+};
+
+
+
+
+
+const empleados: Empleado[] = [
+    {
+        nombre: "José",
+        edad: 22,
+        activo: true
+    },
+    {
+        nombre: "María",
+        edad: 30,
+        activo: false
+    }
+];
+
+console.log(empleados);
+
